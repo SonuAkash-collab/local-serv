f14cf18350a464bec670c9a4c4000c8d6a92e756
Sonu Akash <akashsonu191@gmail.com>
2026-03-22 14:41:29 +0530

    watcher.py classifies file put on upload - pipeline complate

--- server/watcher.py
+++ server/watcher.py
@@ -2,14 +2,21 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from extractor import extract_text
+import joblib
+from embedder import get_embedding
+
+model = joblib.load(r"C:\Users\akash\Documents\Local_serv\models\classifier.pkl")
+
 
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         if event.src_path.endswith(".pdf"):
             time.sleep(1)
-            path=event.src_path
-            text=extract_text(path)
-            print(text)
+            path = event.src_path
+            text = extract_text(path)
+            embedding = get_embedding(text)
+            category = model.predict([embedding])[0]
+            print(f"File: {path} → Category: {category}")
 
 folder_to_watch = r"C:\Users\akash\Documents\Local_serv\uploads"
 
